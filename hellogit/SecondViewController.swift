40e748590ba0a55f9c84d4c154304a11efc6f949
student <s528803@ch140028m.nwmissouri.edu>
2017-10-02 12:07:52 -0500

    Initial Commit

---
+++ hellogit/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  hellogit
+//
+//  Created by Kalvakota,Vaishnavi on 10/2/17.
+//  Copyright © 2017 Kalvakota,Vaishnavi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
